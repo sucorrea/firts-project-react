5dee5e538192c3d58063280329cf78dcef553a31
Sueli Correa de Oliveira <oliver.sueli@gmail.com>
2025-07-25 16:52:51 -0300

    Refactor code structure for improved readability and maintainability

--- src/App.tsx
+++ src/App.tsx
@@ -13,12 +13,18 @@ function App() {
   }, [])
 
   return (
-    <div>
+    <div className="flex border-2 rounded-2xl flex-wrap justify-center items-center p-4  max-w-screen-xl mx-auto h-screen">
+      <h1 className="w-full text-center text-2xl font-bold">Pokemons</h1>
       {pokemon.map((pokemon) => (
-        <div key={pokemon.id} className="card">
-          <h3>{`#000${pokemon.id}`}</h3>
+        <div
+          key={pokemon.id}
+          className="flex flex-col items-center border p-4 m-2 rounded-lg shadow-lg bg-white bg-gradient-to-bl from-blue-100 to-blue-400 hover:shadow-amber-200 transition-shadow duration-300 "
+        >
+          <h3 className="text-black font-bold">{`#000${pokemon.id}`}</h3>
           <img src={pokemon.url} alt={pokemon.name} />
-          <h2>{pokemon.name}</h2>
+          <p className="text-black font-bold text-lg capitalize">
+            {pokemon.name}
+          </p>
         </div>
       ))}
     </div>
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+//create file config tailwind.config.js
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
